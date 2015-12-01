36a30099b6a6e3da0b682ca4a48ab9d52403a242
Michael Miljanovic <michael.miljanovic@uoit.ca>
2015-11-30 19:57:20 -0500

    newest version

--- Assets/Scripts/GenericBug.cs
+++ Assets/Scripts/GenericBug.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
 
 public class GenericBug : MonoBehaviour {
 		public Animator anim;
 		public bool dead = false;
 		public bool finished = false;
 	public GameObject codescreen;
+
+	float initialLineY = 3.5f;
+	float linespacing = 0.825f;
 		
 		// Use this for initialization
 		void Start () {
@@ -19,6 +23,9 @@ public class GenericBug : MonoBehaviour {
 		
 		void OnTriggerEnter2D(Collider2D p){
 			if (p.name == "projectileBug(Clone)") {
+			StreamWriter sw = new StreamWriter("toollog.txt",true);
+			sw.WriteLine("BugCaught,"+((int)((initialLineY-this.transform.position.y)/linespacing)).ToString()+","+Time.time.ToString());
+			sw.Close();
 				this.GetComponent<Renderer>().enabled = true;
 				Destroy (p.gameObject);
 				anim.SetBool("Dying", true);
--- Assets/Scripts/warper.cs
+++ Assets/Scripts/warper.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
 
 public class warper : MonoBehaviour {
 
 	public GameObject CodeScreen;
-
 	public string filename;
+	public string linenum = "";
+	public GameObject selectTools;
+	public int[] tools = {0,0,0,0,0,0};
+	bool toolgiven = false;
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
@@ -18,9 +21,22 @@ public class warper : MonoBehaviour {
 	}
 	void OnTriggerEnter2D(Collider2D c){
 		if (c.name == "projectileWarp(Clone)"){
+			StreamWriter sw = new StreamWriter("toollog.txt",true);
+			sw.WriteLine("Warped,"+filename+","+Time.time.ToString());
+			sw.Close();
 			Destroy(c.gameObject);
 			LevelGenerator lg = CodeScreen.GetComponent<LevelGenerator>();
-			lg.BuildLevel(filename);
+			if(!toolgiven){
+				toolgiven = true;
+				for (int i = 0;i<6;i++){
+					if (tools[i]>0){
+						selectTools.GetComponent<SelectedTool>().toolget = true;
+					}
+					selectTools.GetComponent<SelectedTool>().toolCounts[i] += tools[i];
+				}
+			}
+
+			lg.BuildLevel(@"leveldata\" + filename, true, linenum);
 		}
 	}
 }
